370e5a6a1587ee796f5f3392238ef60d3cb7b925
Marcos Vinicius <contato.marcoscandido@gmail.com>
2024-02-01 14:21:28 -0300

    OAuth2 Facebook

---
+++ src/main/java/com/dev/OAuth2facebook/OAuth2facebookApplication.java
@@ -0,0 +1,13 @@
+package com.dev.OAuth2facebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuth2facebookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OAuth2facebookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/OAuth2facebook/OAuth2facebookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.OAuth2facebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuth2facebookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
